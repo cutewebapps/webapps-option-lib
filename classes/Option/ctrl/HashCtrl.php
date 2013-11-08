3d32a460f0dedfdad0773e58642b610ed8fe9749
Victor Lopata <webmaster@cutewebapps.com>
2013-11-08 07:42:32 -0500

    option/hash/index

--- classes/Option/ctrl/HashCtrl.php
+++ classes/Option/ctrl/HashCtrl.php
@@ -2,9 +2,6 @@
 
 class Option_HashCtrl extends App_AbstractCtrl
 {
-    public function indexAction()
-    {
-    }
 
     public function setAction()
     {
@@ -15,6 +12,24 @@ class Option_HashCtrl extends App_AbstractCtrl
                 $this->_getParam( 'key' ),
                 $this->_getParam( 'value' )
                 );
+        }
+    }
+
+    public function indexAction()
+    {
+        $arrFields = is_object( App_Application::getInstance()->getConfig()->options ) ?
+			App_Application::getInstance()->getConfig()->options->toArray() : array();
+
+        if ( $this->_isPost() ) {
+            foreach ( $arrFields as $strField ) {
+                if ( $this->_hasParam( $strField )) {
+                    Option_Hash::set( $strField, $this->_getParam( $strField ) );
+                }
+            }
+        }
+        
+        foreach ( $arrFields as $strField ) {
+            $this->view->$strField  = Option_Hash::get( $strField );
         }
     }
 }
